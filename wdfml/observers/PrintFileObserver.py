92c1bb14d0124866d80cd11411207c8bc5f40eed
Elena Cuoco <elena.cuoco@gmail.com>
2017-06-10 18:50:35 +0200

    added mongodb observer

--- wdfml/observers/PrintFileObserver.py
+++ wdfml/observers/PrintFileObserver.py
@@ -18,6 +18,7 @@ class PrintTriggers(Observer):
     def __init__(self, par):
         self.filesave = par.outdir + 'WDFTrigger-%s-GPS%s-AR%s-Win%s-Ov%s-SNR%s.csv' % (
             par.chan, int(par.gpsStart), par.ARorder, par.window, par.overlap, int(par.threshold))
+
         try:
             os.remove(self.filesave)
         except OSError:
@@ -26,10 +27,10 @@ class PrintTriggers(Observer):
 ### write on disk in ordered way
     def update(self, Cev):
         self.file_exists = os.path.isfile(self.filesave)
-        PrCev = Cev.__dict__.copy()
+        self.CEV = Cev.__dict__
         with open(self.filesave, 'a') as csvfile:
-            headers = sorted(PrCev.keys(), key=lambda x:x[:3])
+            headers = sorted(self.CEV.keys(), key=lambda x:x[:3])
             writer = csv.DictWriter(csvfile, delimiter=',', lineterminator='\n', fieldnames=headers)
             if not self.file_exists:
                 writer.writeheader()
-            writer.writerow(PrCev)
+            writer.writerow(self.CEV)
